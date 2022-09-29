64452bdc18356b00d5b73059783e49b40dbeee8b
alexeirttd <62844779+alexeirttd@users.noreply.github.com>
2022-09-29 14:35:40 +0300

    Update web.php

--- HR/routes/web.php
+++ HR/routes/web.php
@@ -16,3 +16,15 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('main');
 });
+
+Route::get('/full_list', function () {
+    return view('full_list');
+});
+
+Route::get('/relevant_list', function() {
+    return view('relevant_list');
+});
+
+Route::get('/about', function () {
+    return view('about');
+});
