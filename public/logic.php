cd4f9b09df273fb3bb5465554359decc1ad05af9
Your Name <youremail@yourdomain.com>
2022-12-20 20:49:16 +0300

    Изменил logic.php

--- public/logic.php
+++ public/logic.php
@@ -1,32 +1,4 @@
 <?php
-//$array = [];
-$cityArray = [];
-$enrollmentArray = [];
-$edu_levelArray = [];
-$majorArray = [];
-$experienceArray = [];
-$company_tArray = [];
-$company_sArray = [];
-$last_jobArray = [];
-$genderArray = [];
-$rel_experienceArray = [];
-//
-//if (key_exists('submit_btn')) {
-//
-//    $array['city'] = $_GET['city'];
-//    $array['enrollment'] = $_GET['enrollment'];
-//    $array['edu_level'] = $_GET['edu_level'];
-//    $array['major'] = $_GET['major'];
-//    $array['experience'] = $_GET['experience'];
-//    $array['company_t'] = $_GET['company_t'];
-//    $array['company_s'] = $_GET['company_s'];
-//    $array['last_job'] = $_GET['last_job'];
-//    $array['gender'] = $_GET['gender'];
-//    $array['exp'] = $_GET['exp'];
-//
-//}
-//$array = json_encode($array);
-
 
 function getCity()
 {
