19de7d5f30e488fbe79477e66bb4836e66fc6633
Your Name <youremail@yourdomain.com>
2022-12-21 00:05:17 +0300

    Не доделанная страница "Контакты"

---
+++ contact.php
@@ -0,0 +1,29 @@
+<?php
+require_once "header.php";
+?>
+<body>
+<div class="container contact-sign">
+    <h1 class="text-center">THIS PROJECT WAS CREATED BY:</h1>
+</div>
+
+<div class="row dev-left-col">
+
+    <div class="col-6 col-sm-3 ms-5">Team Leader: Anton Abramov<br>Telegram: @oneofrice</div>
+    <div class="col-6 col-sm-3 ms-5">Fullstack-developer: Alexey Silchenko<br>Telegram: @oneofrice</div>
+    <div class="col-6 col-sm-3 ms-5">ML-developer: Maxim Dorofeev<br>Telegram: @oneofrice</div>
+
+    <!-- Force next columns to break to new line at md breakpoint and up -->
+    <div class="w-100 d-none d-md-block"></div>
+
+    <div class="col-6 col-sm-3 ms-5">Web-designer: Elena Ivanishina<br>Telegram: @oneofrice</div>
+    <div class="col-6 col-sm-3 ms-5">Web-designer: Victoria Bezlutskaya<br>Telegram: @oneofrice</div>
+    <div class="col-6 col-sm-3 ms-5">Analyst: Vadim Gordeev<br>Telegram: @oneofrice</div>
+
+    <div class="w-100 d-none d-md-block"></div>
+
+    <div class="col-6 col-sm-3 ms-5">Frontend-developer: Vadim Kuleshov<br>Telegram: @oneofrice</div>
+    <div class="col-6 col-sm-3 ms-5">Frontend-developer: Tikhon Ognev<br>Telegram: @oneofrice</div>
+    <div class="col-6 col-sm-3 ms-5">Frontend-developer: Nikita Semisotov<br>Telegram: @oneofrice</div>
+</div>
+
+</body>
