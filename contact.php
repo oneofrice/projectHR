a53269639437d6c214b5bee93f9c4827941e948a
Your Name <youremail@yourdomain.com>
2022-12-22 00:37:28 +0300

    Изменил contact.php

--- contact.php
+++ contact.php
@@ -4,26 +4,27 @@ require_once "header.php";
 <body>
 <div class="container contact-sign contact-space">
     <h1 class="text-center"><strong> PROJECT WAS CREATED BY:</strong></h1>
+
+
+    <div class="row contact-space">
+        <div class="col-4">
+            <div class="color-contact">Team Leader: Anton Abramov<br>Telegram: <a href="https://t.me/oneofrice">@oneofrice</a></div>
+            <div class="color-contact">Fullstack-developer: Alexey Silchenko<br>Telegram: <a href="https://t.me/alexei_esly_shto">@alexei_esly_shto</a></div>
+            <div class="color-contact">ML-developer: Maxim Dorofeev<br>VK: <a href="https://vk.com/id442280356">@id442280356</a></div>
+        </div>
+            <!-- Force next columns to break to new line at md breakpoint and up -->
+
+        <div class="col-4">
+            <div class="color-contact">Web-designer: Elena Ivanishina<br>Telegram: <a href="https://t.me/choose35">@choose35</a></div>
+            <div class="color-contact">Web-designer: Victoria Bezlutskaya<br>Telegram: <a href="https://t.me/hezebr">@hezebr</a></div>
+            <div class="color-contact">Analyst: Vadim Gordeev<br>Telegram: <a href="https://t.me/loybf1">@loybf1</a></div>
+        </div>
+
+        <div class="col-4">
+            <div class="color-contact">Frontend-developer: Vadim Kuleshov<br>Telegram: <a href="https://t.me/Dazai405">@Dazai405</a></div>
+            <div class="color-contact">Frontend-developer: Tikhon Ognev<br>VK: <a href="https://vk.com/90deach">@90deach</a></div>
+            <div class="color-contact">Frontend-developer: Nikita Semisotov<br>Telegram: <a href="https://t.me/Sea_F0X">@Sea_F0X</a></div>
+        </div>
+    </div>
 </div>
-
-<div class="row contact-space">
-
-    <div class="col-6 col-sm-3 color-contact">Team Leader: Anton Abramov<br>Telegram: @oneofrice</div>
-    <div class="col-6 col-sm-3 color-contact">Fullstack-developer: Alexey Silchenko<br>Telegram: @oneofrice</div>
-    <div class="col-6 col-sm-3 color-contact">ML-developer: Maxim Dorofeev<br>Telegram: @oneofrice</div>
-
-    <!-- Force next columns to break to new line at md breakpoint and up -->
-    <div class="w-100 d-none d-md-block"></div>
-
-    <div class="col-6 col-sm-3 color-contact">Web-designer: Elena Ivanishina<br>Telegram: @oneofrice</div>
-    <div class="col-6 col-sm-3 color-contact">Web-designer: Victoria Bezlutskaya<br>Telegram: @oneofrice</div>
-    <div class="col-6 col-sm-3 color-contact">Analyst: Vadim Gordeev<br>Telegram: @oneofrice</div>
-
-    <div class="w-100 d-none d-md-block"></div>
-
-    <div class="col-6 col-sm-3 color-contact">Frontend-developer: Vadim Kuleshov<br>Telegram: @oneofrice</div>
-    <div class="col-6 col-sm-3 color-contact">Frontend-developer: Tikhon Ognev<br>Telegram: @oneofrice</div>
-    <div class="col-6 col-sm-3 color-contact">Frontend-developer: Nikita Semisotov<br>Telegram: @oneofrice</div>
-</div>
-
 </body>
